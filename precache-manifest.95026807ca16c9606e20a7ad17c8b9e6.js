de34a52a8652d6e868c2ff70544afa527cb7521b
David Eriksson <david_ux@outlook.com>
2020-05-07 13:54:09 +0200

    Updates

--- precache-manifest.95026807ca16c9606e20a7ad17c8b9e6.js
+++ precache-manifest.95026807ca16c9606e20a7ad17c8b9e6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "37b7a071e226aee68151491b00e71826",
+    "revision": "93edc5cc1d146826b11201884096dc7c",
     "url": "/aktuella/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/aktuella/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "39fcb3e0612deb08d0ac",
+    "revision": "7223c7b6f5df13b9fa5c",
     "url": "/aktuella/static/css/main.5cb06b04.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/aktuella/static/js/2.d64d532c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "39fcb3e0612deb08d0ac",
-    "url": "/aktuella/static/js/main.6926ecb8.chunk.js"
+    "revision": "7223c7b6f5df13b9fa5c",
+    "url": "/aktuella/static/js/main.8b9f91e6.chunk.js"
   },
   {
     "revision": "39321474faccbf392c13",
